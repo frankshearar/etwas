6c099ea2aa9cec5928bedd24cb612ee9ea223113
Frank Shearar <frank.shearar@gmail.com>
2015-05-28 21:13:25 +0100

    When we stop monitoring, we need to shut down the reactive pipelines, or events will try work their way through disposed objects.

--- src/azmon.Lib/Monitor.fs
+++ src/azmon.Lib/Monitor.fs
@@ -4,13 +4,6 @@ open Microsoft.Diagnostics.Tracing
 open Microsoft.Diagnostics.Tracing.Session
 open System.Reactive.Subjects
 
-// Do we need unique sessions for azmon processes? (difficult to clean up)
-// Should we use unique sessions for sets of sources? So should azmon processes
-// listening to Foo and Bar events share the same session? (difficult to clean up)
-// Keeping the session alive's useful for protecting against azmon crashes
-// Do TraceEventSessions survive reboots? (Probably not: other kernel objects like
-// monitors & sockets don't)
-
 // Problems:
 //* Tests are SUPER flaky
 //* Need to separate session creation from session listening: privilege separation
@@ -24,7 +17,9 @@ type Session = {
     with
     interface System.IDisposable with
         member x.Dispose() =
-            x.Subject.Dispose()
+                x.Subject.OnCompleted() // Tell our listeners we're done!
+                x.Subject.Dispose()
+                // Don't Dispose() Trace, because that should live on until we call Monitor.stop.
 
 let (|Null|Value|) (x: System.Nullable<_>) =
     // Apparently GetValueOrDefault is faster, and the Unchecked.defaultof<> cannot
@@ -72,7 +67,8 @@ let createSession name =
 // providers (event source names), and every time we pick up a message, we
 // push it to the returned IObservable.
 //
-// This session is shared across all running azmon processes.
+// This ETW session is shared across all running azmon processes. The Session
+// instance returned will stop sending events when it is Dispose()d.
 let start name (names: string list) =
     let sess = createSession name
     enableProviders names sess
@@ -82,7 +78,7 @@ let start name (names: string list) =
 // Stop monitoring. This disposes the event session, so will stop receiving
 // events for all running azmon processes on this machine. Further, you
 // will not collect events in the session until you call start again.
-let stop session =
+let stop (session: Session) =
     let r = session.Trace.Stop(noThrow = true)
     session.Trace.Dispose()
     r
