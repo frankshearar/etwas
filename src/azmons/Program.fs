3fc8e5b66e7a00e709426fc85397ee91f9c458a2
Frank Shearar <frank.shearar@gmail.com>
2015-05-29 21:53:29 +0100

    Initial stub of the server side, azmons.exe.

---
+++ src/azmons/Program.fs
@@ -0,0 +1,29 @@
+﻿module azmons
+
+open Nessos.UnionArgParser
+
+type Arguments =
+    | [<EqualsAssignment>] Port of int
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Port _ -> "Listen on a particular port. Defaults to 8080"
+
+let parser = UnionArgParser.Create<Arguments>()
+let usage = parser.Usage()
+
+[<EntryPoint>]
+let main argv =
+    try
+        if Array.isEmpty argv then
+            printfn "%s" usage
+            0
+        else
+            0
+    with
+    | :? System.ArgumentException as e ->
+        printfn "%s" usage
+        printfn "-------"
+        printfn "%A" (e.ToString())
+        1
