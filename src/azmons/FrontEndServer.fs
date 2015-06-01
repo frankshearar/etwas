258587fc8f4ecf37837361ba6d05f740a5f397f7
Frank Shearar <frank.shearar@gmail.com>
2015-06-01 21:05:39 +0100

    Stub 'front end' hub for external clients.

---
+++ src/azmons/FrontEndServer.fs
@@ -0,0 +1,27 @@
+﻿module FrontEndServer
+
+open AsyncExtensions
+open Microsoft.AspNet.SignalR
+open Microsoft.AspNet.SignalR.Hubs
+open System
+open System.Reactive.Subjects
+
+let sendAll msg =
+    async {
+        GlobalHost.ConnectionManager.GetConnectionContext<_>().Connection.Broadcast msg
+        |> awaitTask
+        |> ignore
+    } |> Async.RunSynchronously
+
+[<HubName("display")>]
+type DisplayHub(subject: Subject<string>) =
+    inherit Hub()
+    // I don't want to expose events, but I can't figure out how to make
+    // a private variable that's still accessible in Dispose(). It's _supposed_
+    // to just happen automatically.
+    let events = Observable.subscribe (fun s -> sendAll s) subject
+    member x.Events with get() = events
+    with
+    interface IDisposable with
+        member x.Dispose() =
+            x.Events.Dispose()
