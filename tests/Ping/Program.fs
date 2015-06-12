85ff847112b10b747aef143531b88bde895dacb7
Frank Shearar <frank.shearar@gmail.com>
2015-06-12 20:19:00 +0100

    Ping.exe now takes --interval, the time-to-wait in milliseconds between event creation.

--- tests/Ping/Program.fs
+++ tests/Ping/Program.fs
@@ -1,5 +1,15 @@
-﻿open System
+﻿open Nessos.UnionArgParser
+open System
 open System.Threading;
+
+type Arguments =
+    | Interval of int
+    with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Interval _ -> "Time between events. Default: 1s"
+
 
 let registerExitOnCtrlC (canceller: CancellationTokenSource) =
     Console.CancelKeyPress
@@ -9,18 +19,35 @@ let registerExitOnCtrlC (canceller: CancellationTokenSource) =
         canceller.Cancel())
     |> ignore
 
+let parser = UnionArgParser.Create<Arguments>()
+let usage = parser.Usage()
 
 [<EntryPoint>]
 let main argv =
-    let canceller = new CancellationTokenSource()
+    try
+        let args = parser.Parse argv
 
-    let runUntilCancelled = async {
-        registerExitOnCtrlC canceller
+        let args = parser.Parse (argv, raiseOnUsage = false)
+        if args.IsUsageRequested then
+            printfn "%s" usage
+            0
+        else
+            let millis = args.GetResult (<@ Interval @>, defaultValue = 1000)
 
-        while true do
-            do! Async.Sleep(1000)
-            Ping.ping.Ping()
-            printfn "Ping!"
-    }
-    Async.RunSynchronously(runUntilCancelled, 10000, canceller.Token)
-    0
+            let canceller = new CancellationTokenSource()
+
+            let runUntilCancelled = async {
+                registerExitOnCtrlC canceller
+
+                while true do
+                    do! Async.Sleep(millis)
+                    Ping.ping.Ping()
+            }
+            Async.RunSynchronously(runUntilCancelled, 10000, canceller.Token)
+            0
+    with
+    | :? System.ArgumentException as e ->
+        printfn "%s" usage
+        printfn "-------"
+        printfn "%A" (e.ToString())
+        1
