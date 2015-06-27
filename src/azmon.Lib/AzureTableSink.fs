3913e98366dc9d21dd5d7e5821f4de73c07f461c
Frank Shearar <frank.shearar@gmail.com>
2015-06-27 15:15:29 +0100

    Add the table sink itself. Update the README.

---
+++ src/azmon.Lib/AzureTableSink.fs
@@ -0,0 +1,25 @@
+﻿module AzureTableSink
+
+open Microsoft.Diagnostics.Tracing
+open System.Diagnostics.Tracing
+open Microsoft.Practices.EnterpriseLibrary.SemanticLogging
+open Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Schema
+open System
+open System.Collections.ObjectModel
+
+let translateToInProc(e: TraceEvent): EventEntry =
+    let payloadLength = Array.length e.PayloadNames
+    let payload = new ReadOnlyCollection<_>(Array.init payloadLength e.PayloadValue)
+    let schema = new EventSchema(int(e.ID),
+                                 e.ProviderGuid,
+                                 e.ProviderName,
+                                 enum<EventLevel>(int(e.Level)),
+                                 enum<EventTask>(int(e.Task)),
+                                 e.TaskName,
+                                 enum<EventOpcode>(int(e.Opcode)),
+                                 e.OpcodeName,
+                                 (*e.Keywords*)Unchecked.defaultof<EventKeywords>, // TODO: Fix Keyword translation
+                                 "keywordsDescription",                            // TODO: Fix Keyword translation
+                                 e.Version,
+                                 e.PayloadNames)
+    new EventEntry(Guid.Empty, int(e.ID), e.FormattedMessage, payload, new DateTimeOffset(e.TimeStamp), schema)
